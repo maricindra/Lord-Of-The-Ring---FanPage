e785d8824c487f497f65f3ee131be0ff659c48f2
maricindra <marinacindra13@gmail.com>
2026-02-23 22:32:34 -0300

    footer update

--- src/components/Home_elements/Squad_topics.jsx
+++ src/components/Home_elements/Squad_topics.jsx
@@ -3,7 +3,7 @@ function Squad_topics() {
     {
       id: 1,
       icon: (
-        <svg className="w-32 h-32" viewBox="0 0 24 24" fill="none" stroke="#4f6146" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+        <svg className="w-28 h-28" viewBox="0 0 24 24" fill="none" stroke="#4f6146" strokeWidth="0.8" strokeLinecap="round" strokeLinejoin="round">
           <path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6" />
           <path d="M18 9h1.5a2.5 2.5 0 0 0 0-5H18" />
           <path d="M4 22h16" />
@@ -17,24 +17,33 @@ function Squad_topics() {
     },
     {
       id: 2,
-      // Se você quer que o ícone seja um link, mantenha assim:
       icon: (
         <a href="https://www.youtube.com/watch?v=yqKThTGi6I8&list=PL00F32DF9392A2667" target="_blank" rel="noopener noreferrer ">
-          <svg className="w-32 h-32" viewBox="0 0 24 24" fill="none" stroke="#4f6146" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M9 18V5l12-2v13" />
-            <circle cx="6" cy="18" r="3" />
-            <circle cx="18" cy="16" r="3" />
-          </svg>
+          <svg className="w-32 h-32" viewBox="0 0 128 128" fill="none" stroke="#4f6146"  strokeWidth="4" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M38 24V72M38 24C50 24 55 35 45 40" fill="none" />
+            <circle cx="26" cy="72" r="12" />
+            <path d="M70 40V88 M115 30V78 M70 40L115 30" fill="none" />
+            <circle cx="58" cy="88" r="12" />
+            <circle cx="103" cy="78" r="12" />
+        </svg>
         </a>
       ),
-      text : "The best Soundtrack",
+      text :"The best Soundtrack",
     },
     {
       id: 3,
       icon: (
-        <svg className="w-32 h-32" viewBox="0 0 128 128" fill="#4f6146" stroke="#4f6146" strokeWidth="0" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M107 80A12 12 0 1 0 107 104 12 12 0 1 0 107 80zM57 90A12 12 0 1 0 57 114 12 12 0 1 0 57 90zM22 60A12 12 0 1 0 22 84 12 12 0 1 0 22 60z"/>
-        <path d="M33.9 24c-1.6 0-2.9 1.4-2.9 3l0 31.2c0 .8-.8 1.2-1.5.8-2.7-1.6-6-2.3-9.4-1.9-6.8.9-12.3 6.4-13 13.3-1 9.4 6.7 17.2 16 16.6C31 86.4 37 79.5 37 71.6l0-41.1c3.5 1.2 6 4.5 6 8.4 0 1.2.7 2.4 1.9 2.8C47 42.6 49 41 49 39 49 30.7 42.2 23.9 33.9 24zM22 81c-5 0-9-4-9-9s4-9 9-9 9 4 9 9S27 81 22 81zM122 49.2c0-4.5-2.3-8.7-6.2-11.1-2.9-1.8-6.3-2.2-9.6-1.6l-29.7 5.9C70.4 43.7 66 49 66 55.2v33c0 .8-.8 1.2-1.5.8-2.7-1.6-6-2.3-9.4-1.9-6.8.9-12.3 6.4-13 13.3-1 9.4 6.7 17.2 16 16.6 7.9-.6 13.9-7.4 13.9-15.3l0-32.2 44-8.8v17.5c0 .8-.8 1.2-1.5.8-2.7-1.6-6-2.3-9.4-1.9-6.8.9-12.3 6.4-13 13.3-1 9.4 6.7 17.2 16 16.6 7.9-.6 13.9-7.4 13.9-15.3L122 49.2zM57 111c-5 0-9-4-9-9s4-9 9-9 9 4 9 9S62 111 57 111zM72 63.3v-8.1c0-3.3 2.4-6.2 5.6-6.9l30-6c2.1-.4 4.2.1 5.8 1.5 1.6 1.3 2.6 3.3 2.6 5.4v5.3L72 63.3zM107 101c-5 0-9-4-9-9s4-9 9-9 9 4 9 9S112 101 107 101z"/>
+        <svg className="w-28 h-28" viewBox="0 0 128 128" fill="none" stroke="#4f6146" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
+          <path d="M117 27c0 48-21 64-32 64s-32-16-32-64c0-11 11-16 32-16s32 5 32 16z" />
+          <path d="M93 53c-4 4-11 4-15 0" /> {/* Sorriso */}
+          <circle cx="75" cy="35" r="1.5" fill="currentColor" /> {/* Olho Esquerdo */}
+          <circle cx="95" cy="35" r="1.5" fill="currentColor" /> {/* Olho Direito */}
+          
+          {/* Máscara de Fundo (Esquerda) */}
+          <path d="M54 38c-6 1-13 4-22 9-19 11-25 21-20 30 24 41 51 45 60 40 5-3 10-11 10-25" />
+          <path d="M49 88c2-6 7-9 13-7" /> {/* Boca Triste */}
+          <circle cx="35" cy="60" r="1.5" fill="currentColor" /> {/* Olho Esquerdo */}
+          <circle cx="50" cy="55" r="1.5" fill="currentColor" /> {/* Olho Direito */}
         </svg>
       ),
       text: "The best Cinematography, Visual Effects and Makeup",
@@ -43,7 +52,7 @@ function Squad_topics() {
 
   return (
     <div className="max-w-6xl mx-auto py-10 text-center md:flex-row gap-4  ">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
+      <div className="grid grid-col md:flex-row md:grid-cols-3 gap-4 ">
         {topics.map((topic) => (
           <div
             key={topic.id}
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Playlist from './Playlist';
+import Spline from '@splinetool/react-spline';
+
 
 function Footer() {
   return (
-<div>
 
-
-    <h1 className="text-golden_old font-cocogothic tracking-[0.4em] uppercase">Footer</h1>
+      <div className="w-50 h-50 md:w-100 md:h-100 justify-center items-center flex flex-col mx-auto ">
+        <h1 className="text-golden_old center font-cocogothic tracking-[0.4em] uppercase">Lord of the ring</h1>
+        <Spline
+        scene="https://prod.spline.design/dEMJaIsLBCWJx-2w/scene.splinecode" />
+      
     <Playlist/>
-
 </div>
   );
 }
